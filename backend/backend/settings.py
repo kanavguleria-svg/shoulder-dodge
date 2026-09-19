6d055a59d9ba244d244694a7d2be5cd855d81ee7
kanav guleria <kanav.guleria18@hotmail.com>
2026-09-19 15:03:34 +0530

    Configure auto-migrations and connection pooling for Postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL and dj_database_url:
     DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, conn_health_checks=True)
     }
 else:
     DATABASES = {
