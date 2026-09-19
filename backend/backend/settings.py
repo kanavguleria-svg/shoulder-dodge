6a8877e200021062e6ed8635ab62d84115ad89c9
kanav guleria <kanav.guleria18@hotmail.com>
2026-09-19 15:17:48 +0530

    fix: fixed error duplicate string [MAT]

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,9 +98,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL and dj_database_url:
     DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
-        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, conn_health_checks=True)
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600),
     }
+    DATABASES['default']['CONN_HEALTH_CHECKS'] = True
 else:
     DATABASES = {
         'default': {
